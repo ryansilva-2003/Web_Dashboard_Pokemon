533626de4c8931799d09ba0bae50256ca52cb52a
Ryan <ryansilvadeoliveira53@gmail.com>
2025-10-12 00:23:54 -0300

    Adição das imagens home

--- script.js
+++ script.js
@@ -9,18 +9,9 @@ selectBtn.addEventListener("click", () => {
 
 typeItems.forEach(item => {
     item.addEventListener("click", () => {
-        const checked = document.querySelectorAll(".item.checked");
-
-        if (!item.classList.contains("checked") && checked.length >= 2) {
-            alert("Você só pode selecionar até 2 tipos.");
-            return;
-        }
-
-
         item.classList.toggle("checked");
-
-        const newChecked = document.querySelectorAll(".item.checked");
-        btnText.innerText = newChecked.length > 0 ? `${newChecked.length} Selecionado(s)` : "Selecionar tipo V";
+        const checked = document.querySelectorAll(".item.checked");
+        btnText.innerText = checked.length > 0 ? `${checked.length} Selecionado(s)` : "Selecionar tipo";
     });
 });
 
@@ -34,17 +25,9 @@ selector.addEventListener("click", () => {
 
 genItems.forEach(iten => {
     iten.addEventListener("click", () => {
-        const checked = document.querySelectorAll(".iten.checked");
-
-        if (!iten.classList.contains("checked") && checked.length >= 2) {
-            alert("Você só pode selecionar até 2 gerações.");
-            return;
-        }
-
         iten.classList.toggle("checked");
-
-        const newChecked = document.querySelectorAll(".iten.checked");
-        btbText.innerText = newChecked.length > 0 ? `${newChecked.length} Selecionado(s)` : "Selecionar geração V";
+        const checked = document.querySelectorAll(".iten.checked");
+        btbText.innerText = checked.length > 0 ? `${checked.length} Selecionados` : "Selecionar geração V";
     });
 });
 
